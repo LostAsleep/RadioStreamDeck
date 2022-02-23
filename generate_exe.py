a3831011aac661d787a1e34822ca45c70a48dc40
LostAsleep <66181322+LostAsleep@users.noreply.github.com>
2022-02-23 13:37:08 +0100

    Add DLL and Assets folder into binary bundle.

--- generate_exe.py
+++ generate_exe.py
@@ -1,8 +1,30 @@
+"""Generate a bundled binary from the main.py with included usblib DLL and Assets folder.
+
+This is the expected file / folder structure:
+
+current_directory
+ | - main.py
+ | - hidapi.dll
+ | - hidapi.lib
+ | - hidapi.pdb
+ | - Assets
+   | - Roboto-Regular.ttf
+   | - Exit.png
+   | - Pressed.png
+   | - Released.png
+
+"""
+
 import PyInstaller.__main__
+
 
 PyInstaller.__main__.run([
     'main.py',
-    #'--onefile',
-    #'--noconsole',
-    #'--windowed'
+    '--clean',
+    '--onefile',
+    '--add-binary=hidapi.dll;.',
+    '--add-binary=hidapi.lib;.',
+    '--add-binary=hidapi.pdb;.',
+    '--add-data=Assets;Assets',
+    #'--noconsole'  # For final version
 ])
